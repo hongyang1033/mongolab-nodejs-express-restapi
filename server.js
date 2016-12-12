f498e7b02626d5bc8e1cb39268ab1d3fa9c761fa
hongyang1033 <hongyang1033@yahoo.com>
2016-12-12 05:02:36 +0000

    Second Commit

--- server.js
+++ server.js
@@ -61,4 +61,7 @@ app.post('/api/posts', function(req, res) {
 });
 
 
-app.listen(3000);
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
